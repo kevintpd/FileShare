b8541bd9effffb3ab70cc7e974fc3db94f72ed33
TanPengda <1413085812@qq.com>
2022-03-09 22:44:28 +0800

    revise models.py in folder and file. some problem in attribute.

--- fileshare/file/models.py
+++ fileshare/file/models.py
@@ -2,4 +2,14 @@ from django.db import models
 
 # Create your models here.
 class file(models.Model):
+    #文件模型类
+    #文件名
     filename = models.CharField('文件名',max_length=20)
+    #文件所属文件夹
+    belongtofolder = models.CharField('文件所属文件夹',max_length=20, default='')
+    #文件大小,单位kb
+    filesize = models.DecimalField('文件大小',max_digits=10,decimal_places=2,default='')
+    #文件类型
+    filetype = models.CharField('文件类型',max_length=10, default='')
+    #文件下载次数
+    downloadtimes = models.IntegerField('文件下载次数',default='')
--- fileshare/folder/models.py
+++ fileshare/folder/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 # Create your models here.
-class ShareFolder(models.Model):
+class Folder(models.Model):
     #创建分享文件夹模型
     #文件夹名称
     foldername = models.CharField("文件夹名",max_length=20)
@@ -11,16 +11,10 @@ class ShareFolder(models.Model):
     exist_time = models.IntegerField("存在时间")
     #文件夹所属人
     # belongtouser = models.ForeignKey()
-    #文件夹分享码
+    #文件夹创建者
+    foldercreater = models.CharField("文件夹创建者",max_length=20)
+    #文件夹分享码,有分享码则为 共享文件夹 分享码为空就是普通文件夹
     sharecode = models.CharField("分享码",max_length=20)
+    #文件夹权限 1代表可以上传 2代表可以删除 4代表可以下载 3代表上传和删除 5代表上传下载 6删除下载 7上传删除下载
+    folderpermission = models.IntegerField()
 
-class CommonFolder(models.Model):
-    #创建非分享文件夹
-    # 文件夹名称
-    foldername = models.CharField("文件夹名", max_length=20)
-    # 文件夹创建时间
-    create_time = models.DateTimeField("创建时间", auto_now_add=True)
-    # 文件夹存在时长
-    exist_time = models.IntegerField("存在时间")
-    # 文件夹所属人
-    # belongtouser = models.ForeignKey()
